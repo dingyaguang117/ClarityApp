20d70778cced066a0900354194069307f1f63fed
YaguangDing <dingyaguang117@gmail.com>
2019-01-10 00:37:57 +0800

    Initial Commit

---
+++ ClarityApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ClarityApp
+//
+//  Created by Yaguang Ding on 2019/1/10.
+//  Copyright © 2019 Yaguang Ding. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
